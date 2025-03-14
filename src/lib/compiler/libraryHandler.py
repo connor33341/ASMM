dba91ad8ba7d488d372ac4363301f2f460a967ad
Connor <107011324+connor33341@users.noreply.github.com>
2025-03-14 17:03:49 +0000

    Library Handler Push

--- src/lib/compiler/libraryHandler.py
+++ src/lib/compiler/libraryHandler.py
@@ -1,10 +1,15 @@
 from values.translatorData import TranslatorData
-from lib.compiler.byLines import byLines
+from lib.compiler.byLines import ByLines
 from utils.globalLogger import log
 
 class LibraryHandler:
     def __init__(self,Data: TranslatorData):
         self.Data = Data
-        self.LineReader = ByLines()
-
+        self.Running = True
+        log("Running Library Handler")
     
+    def Main(self):
+        self.CurrentFile = ByLines(self.Data.Code)
+        while self.Running:
+            self.CurrentLine = self.CurrentFile.ReadNext()
+            self.LineIndex = self.CurrentFile.Index
